244febb8e22160983bd323822115170b67ac11de
Kye Berkeland <kyeberkeland@yahoo.com>
2018-12-14 15:52:24 -0600

    Create an array to store employees

--- client.js
+++ client.js
@@ -12,44 +12,47 @@ class Employee{
 
   $ ( document ).ready( function() {
     console.log('DOM ready');
-    // $ ( '#addCarButton' ).on( 'click', addCarToGarage );
+    $ ( '#addEmployeeButton' ).on( 'click', addEmployee );
 
   }); // end document ready
   
   
-//   function newCar( year, make, model, image ){
-//     console.log( 'in newCar:', year, make, model, image );
-//     garage.push( new Car( year, make, model, image ) );
-//     return true;
-//   } // end newCar
+  function newEmployee( firstName, lastName, idNumber, title, annualSalary ){
+    console.log( 'in newEmployee:', firstName, lastName, idNumber, title, annualSalary );
+    employeeArray.push( new Employee( firstName, lastName, parseFloat(idNumber), title, parseFloat(annualSalary) ) );
+    return true;
+  } // end newEmployee
   
-//   // function for adding a car to garage array
-//   function addCarToGarage() {
-//     displayMessage('');
-//     if ( $( '#yearIn' ).val() == '' || $( '#makeIn' ).val() == '' || $( '#modelIn' ).val() == '' || $( '#imageIn' ).val() == '') {
-//       // display an error message because one of the fields is empty
-//       displayMessage('Please try again, there is a value missing.');
-//       return false;
-//     } else {
-//       // use newCar function to add a car
-//       newCar( $( '#yearIn' ).val(), $( '#makeIn' ).val(), $( '#modelIn' ).val(), $( '#imageIn' ).val() );
-//     } // end if
-//     // clear the values in html
-//     $( '#yearIn' ).val( '' );
-//     $( '#makeIn' ).val( '' );
-//     $( '#modelIn' ).val( '' );
-//     $( '#imageIn' ).val( '' );
-//     showGarage();
-//     // disable button if garage is full
-//     if ( (garage.length) == garageFull ) {
-//       $ ( '#yearIn' ).prop( 'disabled', true );
-//       $ ( '#makeIn' ).prop( 'disabled', true );
-//       $ ( '#modelIn' ).prop( 'disabled', true );
-//       $ ( '#imageIn' ).prop( 'disabled', true );
-//       $ ( '#addCarButton' ).prop( 'disabled', true );
-//       displayMessage('The garage is full.');
-//     } // end if
-//   } // end addCarToGarage
+  // function for adding a car to garage array
+  function addEmployee() {
+    // displayMessage('');
+    if ( $( '#firstNameIn' ).val() == '' || $( '#lastNameIn' ).val() == '' || $( '#idNumberIn' ).val() == '' || $( '#titleIn' ).val() == '' || $( '#annualSalaryIn' ).val() == '') {
+      // display an error message because one of the fields is empty
+      displayMessage('Please try again, there is a value missing.');
+      return false;
+    } else {
+      // use newEmployee function to add an employee
+      newEmployee( $( '#firstNameIn' ).val(), $( '#lastNameIn' ).val(), $( '#idNumberIn' ).val(), $( '#titleIn' ).val(), $( '#annualSalaryIn' ).val() );
+    } // end if
+
+    // clear the values in html
+    $( '#firstNameIn' ).val( '' );
+    $( '#lastNameIn' ).val( '' );
+    $( '#idNumberIn' ).val( '' );
+    $( '#titleIn' ).val( '' );
+    $( '#annualSalaryIn' ).val( '' );
+
+    // showGarage();
+    // // disable button if garage is full
+    // if ( (garage.length) == garageFull ) {
+    //   $ ( '#yearIn' ).prop( 'disabled', true );
+    //   $ ( '#makeIn' ).prop( 'disabled', true );
+    //   $ ( '#modelIn' ).prop( 'disabled', true );
+    //   $ ( '#imageIn' ).prop( 'disabled', true );
+    //   $ ( '#addCarButton' ).prop( 'disabled', true );
+    //   displayMessage('The garage is full.');
+    // } // end if
+  } // end addCarToGarage
   
 //   // function to display added car in html garageList
 //   function showGarage() {
